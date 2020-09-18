5b4516dece76434484c6d8ed505464a092ecd0f7
csutyak <cps46@zips.uakron.edu>
2020-09-18 16:03:16 -0400

    added cycles

--- PCR.py
+++ PCR.py
@@ -1,9 +1,15 @@
 INITIALRNA = "atgtttgt ttttcttgtt ttattgccac tagtctctag tcagtgtgtt aatcttacaa ccagaactca attaccccct gcatacacta attctttcac  acgtggtgtt tattaccctg acaaagtttt cagatcctca gttttacatt caactcaggacttgttctta cctttctttt ccaatgttac ttggttccat gctatacatg tctctgggaccaatggtact aagaggtttg ataaccctgt cctaccattt aatgatggtg tttattttgcttccactgag aagtctaaca taataagagg ctggattttt ggtactactt tagattcgaagacccagtcc ctacttattg ttaataacgc tactaatgtt gttattaaag tctgtgaatttcaattttgt aatgatccat ttttgggtgt ttattaccac aaaaacaaca aaagttggatggaaagtgag ttcagagttt attctagtgc gaataattgc acttttgaat atgtctctcagccttttctt atggaccttg aaggaaaaca gggtaatttc aaaaatctta gggaatttgtgtttaagaat attgatggtt attttaaaat atattctaag cacacgccta ttaatttagtgcgtgatctc cctcagggtt tttcggcttt agaaccattg gtagatttgc caataggtattaacatcact aggtttcaaa ctttacttgc tttacataga agttatttga ctcctggtgattcttcttca ggttggacag ctggtgctgc agcttattat gtgggttatc ttcaacctaggacttttcta ttaaaatata atgaaaatgg aaccattaca gatgctgtag actgtgcacttgaccctctc tcagaaacaa agtgtacgtt gaaatccttc actgtagaaa aaggaatctatcaaacttct aactttagag tccaaccaac agaatctatt gttagatttc ctaatattacaaacttgtgc ccttttggtg aagtttttaa cgccaccaga tttgcatctg tttatgcttggaacaggaag agaatcagca actgtgttgc tgattattct gtcctatata attccgcatcattttccact tttaagtgtt atggagtgtc tcctactaaa ttaaatgatc tctgctttactaatgtctat gcagattcat ttgtaattag aggtgatgaa gtcagacaaa tcgctccagggcaaactgga aagattgctg attataatta taaattacca gatgatttta caggctgcgttatagcttgg aattctaaca atcttgattc taaggttggt ggtaattata attacctgtatagattgttt aggaagtcta atctcaaacc ttttgagaga gatatttcaa ctgaaatctatcaggccggt agcacacctt gtaatggtgt tgaaggtttt aattgttact ttcctttacaatcatatggt ttccaaccca ctaatggtgt tggttaccaa ccatacagag tagtagtactttcttttgaa cttctacatg caccagcaac tgtttgtgga cctaaaaagt ctactaatttggttaaaaac aaatgtgtca atttcaactt caatggttta acaggcacag gtgttcttactgagtctaac aaaaagtttc tgcctttcca acaatttggc agagacattg ctgacactactgatgctgtc cgtgatccac agacacttga gattcttgac attacaccat gttcttttggtggtgtcagt gttataacac caggaacaaa tacttctaac caggttgctg ttctttatcaggatgttaac tgcacagaag tccctgttgc tattcatgca gatcaactta ctcctacttggcgtgtttat tctacaggtt ctaatgtttt tcaaacacgt gcaggctgtt taataggggctgaacatgtc aacaactcat atgagtgtga catacccatt ggtgcaggta tatgcgctagttatcagact cagactaatt ctcctcggcg ggcacgtagt gtagctagtc aatccatcattgcctacact atgtcacttg gtgcagaaaa ttcagttgct tactctaata actctattgccatacccaca aattttacta ttagtgttac cacagaaatt ctaccagtgt ctatgaccaagacatcagta gattgtacaa tgtacatttg tggtgattca actgaatgca gcaatcttttgttgcaatat ggcagttttt gtacacaatt aaaccgtgct ttaactggaa tagctgttgaacaagacaaa aacacccaag aagtttttgc acaagtcaaa caaatttaca aaacaccaccaattaaagat tttggtggtt ttaatttttc acaaatatta ccagatccat caaaaccaagcaagaggtca tttattgaag atctactttt caacaaagtg acacttgcag atgctggcttcatcaaacaa tatggtgatt gccttggtga tattgctgct agagacctca tttgtgcacaaaagtttaac ggccttactg ttttgccacc tttgctcaca gatgaaatga ttgctcaatacacttctgca ctgttagcgg gtacaatcac ttctggttgg acctttggtg caggtgctgcattacaaata ccatttgcta tgcaaatggc ttataggttt aatggtattg gagttacacagaatgttctc tatgagaacc aaaaattgat tgccaaccaa tttaatagtg ctattggcaaaattcaagac tcactttctt ccacagcaag tgcacttgga aaacttcaag atgtggtcaaccaaaatgca caagctttaa acacgcttgt taaacaactt agctccaatt ttggtgcaatttcaagtgtt ttaaatgata tcctttcacg tcttgacaaa gttgaggctg aagtgcaaattgataggttg atcacaggca gacttcaaag tttgcagaca tatgtgactc aacaattaattagagctgca gaaatcagag cttctgctaa tcttgctgct actaaaatgt cagagtgtgtacttggacaa tcaaaaagag ttgatttttg tggaaagggc tatcatctta tgtccttccctcagtcagca cctcatggtg tagtcttctt gcatgtgact tatgtccctg cacaagaaaagaacttcaca actgctcctg ccatttgtca tgatggaaaa gcacactttc ctcgtgaaggtgtctttgtt tcaaatggca cacactggtt tgtaacacaa aggaattttt atgaaccacaaatcattact acagacaaca catttgtgtc tggtaactgt gatgttgtaa taggaattgtcaacaacaca gtttatgatc ctttgcaacc tgaattagac tcattcaagg aggagttagataaatatttt aagaatcata catcaccaga tgttgattta ggtgacatct ctggcattaatgcttcagtt gtaaacattc aaaaagaaat tgaccgcctc aatgaggttg ccaagaatttaaatgaatct ctcatcgatc tccaagaact tggaaagtat gagcagtata taaaatggccatggtacatt tggctaggtt ttatagctgg cttgattgcc atagtaatgg tgacaattatgctttgctgt atgaccagtt gctgtagttg tctcaagggc tgttgttctt gtggatcctgctgcaaattt gatgaagacg actctgagcc agtgctcaaa ggagtcaaat tacattacacataa"
 INITIALRNA = INITIALRNA.replace(" ", "")
+CYCLES = 2
+
+PRIMER = "gatgctgtccgtgatccaca"
+REVERSEPRIMER = "cccgccgaggagaattagtc"
 
 class DNAConvert:
-	def __init__(self, DNAStrand):
+	def __init__(self, DNAStrand, threeToFive):
 		self.DNAStrand = DNAStrand
+		self.length = len(DNAStrand)
+		self.threeToFive = threeToFive
 
 	def printStrand(self):
 		print(self.DNAStrand)
@@ -16,26 +22,71 @@ class DNAConvert:
 		CompSequence = CompSequence.replace("c", "g")
 		CompSequence = CompSequence.replace("T", "t")
 		CompSequence = CompSequence.replace("C", "c")
-		newStrand = DNAConvert(CompSequence)
+
+		if self.threeToFive:
+			newStrandThreeToFive = False
+		else:
+			newStrandThreeToFive = True
+
+		newStrand = DNAConvert(CompSequence, newStrandThreeToFive)
 		return newStrand
+
+	def greatPrimerConvert(self):
+		if self.threeToFive:
+			return self.primerConvert()
+		else:
+			return self.ReversePrimerConvert()
+
+	def primerConvert(self):
+		#finds the index of the primer
+		foundIndex = self.DNAStrand.find(PRIMER)
+		#creates a substring from the index on
+		newString = self.DNAStrand[foundIndex::]
+		newStrand = DNAConvert(newString, self.threeToFive)
+		return newStrand
+
+	def ReversePrimerConvert(self):
+		reverseString = self.DNAStrand[::-1]
+		#finds the index of the primer
+		foundIndex = reverseString.find(REVERSEPRIMER)
+		#creates a substring from the index on
+		newString = reverseString[foundIndex::]
+		newString = newString[::-1]
+		newStrand = DNAConvert(newString, self.threeToFive)
+		return newStrand
+
 
 #loops
 
 DNAArray = []
 DNAStrandNum = 2
 
-initialDNA1 = DNAConvert(INITIALRNA)
+initialDNA1 = DNAConvert(INITIALRNA, True)
 initialDNA2 = initialDNA1.findComp()
 
 DNAArray.append(initialDNA1)
 DNAArray.append(initialDNA2)
 
-DNAArray[0].printStrand()
-DNAArray[1].printStrand()
-
 i = 0
-while i < 10:
+while i < CYCLES:
+	workingArray = []
+	loopCtr = 0
+	print("loop " , i)
+	ctr = 0
+	while loopCtr < DNAStrandNum:
+		workingArray.append(DNAArray[loopCtr].greatPrimerConvert())
+		workingStrand = DNAArray[loopCtr].findComp()
+		workingArray.append(workingStrand.greatPrimerConvert())
+		loopCtr += 1
 
-	
 
+	#ending loop
+	DNAArray = workingArray
+	DNAStrandNum = DNAStrandNum * 2
 	i += 1
+
+ctr = 0
+while ctr < DNAStrandNum:
+	print("Strand ", ctr)
+	DNAArray[ctr].printStrand()
+	ctr += 1
