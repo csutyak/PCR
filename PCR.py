e53f99fcd8034d48d6ea73f795c3284b467c57d7
csutyak <cps46@zips.uakron.edu>
2020-09-18 14:57:22 -0400

    add class DNAConvert and started cycle

--- PCR.py
+++ PCR.py
@@ -1,18 +1,41 @@
 INITIALRNA = "atgtttgt ttttcttgtt ttattgccac tagtctctag tcagtgtgtt aatcttacaa ccagaactca attaccccct gcatacacta attctttcac  acgtggtgtt tattaccctg acaaagtttt cagatcctca gttttacatt caactcaggacttgttctta cctttctttt ccaatgttac ttggttccat gctatacatg tctctgggaccaatggtact aagaggtttg ataaccctgt cctaccattt aatgatggtg tttattttgcttccactgag aagtctaaca taataagagg ctggattttt ggtactactt tagattcgaagacccagtcc ctacttattg ttaataacgc tactaatgtt gttattaaag tctgtgaatttcaattttgt aatgatccat ttttgggtgt ttattaccac aaaaacaaca aaagttggatggaaagtgag ttcagagttt attctagtgc gaataattgc acttttgaat atgtctctcagccttttctt atggaccttg aaggaaaaca gggtaatttc aaaaatctta gggaatttgtgtttaagaat attgatggtt attttaaaat atattctaag cacacgccta ttaatttagtgcgtgatctc cctcagggtt tttcggcttt agaaccattg gtagatttgc caataggtattaacatcact aggtttcaaa ctttacttgc tttacataga agttatttga ctcctggtgattcttcttca ggttggacag ctggtgctgc agcttattat gtgggttatc ttcaacctaggacttttcta ttaaaatata atgaaaatgg aaccattaca gatgctgtag actgtgcacttgaccctctc tcagaaacaa agtgtacgtt gaaatccttc actgtagaaa aaggaatctatcaaacttct aactttagag tccaaccaac agaatctatt gttagatttc ctaatattacaaacttgtgc ccttttggtg aagtttttaa cgccaccaga tttgcatctg tttatgcttggaacaggaag agaatcagca actgtgttgc tgattattct gtcctatata attccgcatcattttccact tttaagtgtt atggagtgtc tcctactaaa ttaaatgatc tctgctttactaatgtctat gcagattcat ttgtaattag aggtgatgaa gtcagacaaa tcgctccagggcaaactgga aagattgctg attataatta taaattacca gatgatttta caggctgcgttatagcttgg aattctaaca atcttgattc taaggttggt ggtaattata attacctgtatagattgttt aggaagtcta atctcaaacc ttttgagaga gatatttcaa ctgaaatctatcaggccggt agcacacctt gtaatggtgt tgaaggtttt aattgttact ttcctttacaatcatatggt ttccaaccca ctaatggtgt tggttaccaa ccatacagag tagtagtactttcttttgaa cttctacatg caccagcaac tgtttgtgga cctaaaaagt ctactaatttggttaaaaac aaatgtgtca atttcaactt caatggttta acaggcacag gtgttcttactgagtctaac aaaaagtttc tgcctttcca acaatttggc agagacattg ctgacactactgatgctgtc cgtgatccac agacacttga gattcttgac attacaccat gttcttttggtggtgtcagt gttataacac caggaacaaa tacttctaac caggttgctg ttctttatcaggatgttaac tgcacagaag tccctgttgc tattcatgca gatcaactta ctcctacttggcgtgtttat tctacaggtt ctaatgtttt tcaaacacgt gcaggctgtt taataggggctgaacatgtc aacaactcat atgagtgtga catacccatt ggtgcaggta tatgcgctagttatcagact cagactaatt ctcctcggcg ggcacgtagt gtagctagtc aatccatcattgcctacact atgtcacttg gtgcagaaaa ttcagttgct tactctaata actctattgccatacccaca aattttacta ttagtgttac cacagaaatt ctaccagtgt ctatgaccaagacatcagta gattgtacaa tgtacatttg tggtgattca actgaatgca gcaatcttttgttgcaatat ggcagttttt gtacacaatt aaaccgtgct ttaactggaa tagctgttgaacaagacaaa aacacccaag aagtttttgc acaagtcaaa caaatttaca aaacaccaccaattaaagat tttggtggtt ttaatttttc acaaatatta ccagatccat caaaaccaagcaagaggtca tttattgaag atctactttt caacaaagtg acacttgcag atgctggcttcatcaaacaa tatggtgatt gccttggtga tattgctgct agagacctca tttgtgcacaaaagtttaac ggccttactg ttttgccacc tttgctcaca gatgaaatga ttgctcaatacacttctgca ctgttagcgg gtacaatcac ttctggttgg acctttggtg caggtgctgcattacaaata ccatttgcta tgcaaatggc ttataggttt aatggtattg gagttacacagaatgttctc tatgagaacc aaaaattgat tgccaaccaa tttaatagtg ctattggcaaaattcaagac tcactttctt ccacagcaag tgcacttgga aaacttcaag atgtggtcaaccaaaatgca caagctttaa acacgcttgt taaacaactt agctccaatt ttggtgcaatttcaagtgtt ttaaatgata tcctttcacg tcttgacaaa gttgaggctg aagtgcaaattgataggttg atcacaggca gacttcaaag tttgcagaca tatgtgactc aacaattaattagagctgca gaaatcagag cttctgctaa tcttgctgct actaaaatgt cagagtgtgtacttggacaa tcaaaaagag ttgatttttg tggaaagggc tatcatctta tgtccttccctcagtcagca cctcatggtg tagtcttctt gcatgtgact tatgtccctg cacaagaaaagaacttcaca actgctcctg ccatttgtca tgatggaaaa gcacactttc ctcgtgaaggtgtctttgtt tcaaatggca cacactggtt tgtaacacaa aggaattttt atgaaccacaaatcattact acagacaaca catttgtgtc tggtaactgt gatgttgtaa taggaattgtcaacaacaca gtttatgatc ctttgcaacc tgaattagac tcattcaagg aggagttagataaatatttt aagaatcata catcaccaga tgttgattta ggtgacatct ctggcattaatgcttcagtt gtaaacattc aaaaagaaat tgaccgcctc aatgaggttg ccaagaatttaaatgaatct ctcatcgatc tccaagaact tggaaagtat gagcagtata taaaatggccatggtacatt tggctaggtt ttatagctgg cttgattgcc atagtaatgg tgacaattatgctttgctgt atgaccagtt gctgtagttg tctcaagggc tgttgttctt gtggatcctgctgcaaattt gatgaagacg actctgagcc agtgctcaaa ggagtcaaat tacattacacataa"
 INITIALRNA = INITIALRNA.replace(" ", "")
-print(INITIALRNA)
 
-def findComp(sequence):
-	CompSequence = sequence
-	CompSequence = CompSequence.replace("a", "T")
-	CompSequence = CompSequence.replace("t", "a")
-	CompSequence = CompSequence.replace("g", "C")
-	CompSequence = CompSequence.replace("c", "g")
-	CompSequence = CompSequence.replace("T", "t")
-	CompSequence = CompSequence.replace("C", "c")
-	return CompSequence
+class DNAConvert:
+	def __init__(self, DNAStrand):
+		self.DNAStrand = DNAStrand
 
-print('\n\n\n')
-newSeq = findComp(INITIALRNA)
-print(newSeq)
+	def printStrand(self):
+		print(self.DNAStrand)
 
+	def findComp(self):
+		CompSequence = self.DNAStrand
+		CompSequence = CompSequence.replace("a", "T")
+		CompSequence = CompSequence.replace("t", "a")
+		CompSequence = CompSequence.replace("g", "C")
+		CompSequence = CompSequence.replace("c", "g")
+		CompSequence = CompSequence.replace("T", "t")
+		CompSequence = CompSequence.replace("C", "c")
+		newStrand = DNAConvert(CompSequence)
+		return newStrand
+
+#loops
+
+DNAArray = []
+DNAStrandNum = 2
+
+initialDNA1 = DNAConvert(INITIALRNA)
+initialDNA2 = initialDNA1.findComp()
+
+DNAArray.append(initialDNA1)
+DNAArray.append(initialDNA2)
+
+DNAArray[0].printStrand()
+DNAArray[1].printStrand()
+
+i = 0
+while i < 10:
+
+	
+
+	i += 1
